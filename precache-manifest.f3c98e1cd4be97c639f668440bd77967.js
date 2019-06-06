fa78b5b70a70b91d86dd3c70e5d2bf4609b48992
Chelsea Childers <chelseachilders@chelseas-mbp.attlocal.net>
2019-06-06 12:49:44 -0400

    Updates

---
+++ precache-manifest.f3c98e1cd4be97c639f668440bd77967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07780bf75943d29ff9032c0822c90e4",
+    "url": "/EasyKnockReactApp/index.html"
+  },
+  {
+    "revision": "2e80e6d5e61e67acff2b",
+    "url": "/EasyKnockReactApp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1a23cf9347236ef0443e",
+    "url": "/EasyKnockReactApp/static/css/main.f9dcbd1d.chunk.css"
+  },
+  {
+    "revision": "2e80e6d5e61e67acff2b",
+    "url": "/EasyKnockReactApp/static/js/2.812c32bc.chunk.js"
+  },
+  {
+    "revision": "1a23cf9347236ef0443e",
+    "url": "/EasyKnockReactApp/static/js/main.43f744d5.chunk.js"
+  },
+  {
+    "revision": "85ea272cec64965071af",
+    "url": "/EasyKnockReactApp/static/js/runtime~main.323ac4d9.js"
+  }
+]);
